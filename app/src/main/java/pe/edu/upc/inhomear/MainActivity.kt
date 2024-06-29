c5c00998d1d0aa84e33baef04ca82562b33af25a
Aldo Medina <u202018286@upc.edu.pe>
2024-06-28 20:34:03 -0500

    hotfix: Removed unused import.

--- app/src/main/java/pe/edu/upc/inhomear/MainActivity.kt
+++ app/src/main/java/pe/edu/upc/inhomear/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
